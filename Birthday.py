70c5a674b87cb771500644e8dc7756098e74a27e
Manfred Stocker <manfred.stocker@jetticket.at>
2024-11-04 11:43:18 +0100

    Refactored birthday katta

--- Birthday.py
+++ Birthday.py
@@ -6,23 +6,35 @@ import json
 with open("dataFiles/birthdays.json", "r", encoding="utf-8") as file:
     birthday = json.load(file)
 
-print("Welcome to the birthday dictionary. We know the birthdays of:")  
-for name in birthday:
-    print(name)
+def listNames():
+    print("We know the birthdays of:")
+    for name in birthday:
+        print(name)
 
-print("Who's birthday do you want to look up?")
-name = input()
+def findAndPrintBirthDay():
+    name = input("Who's bithday do you want to find: ")
+    if name in birthday:
+       print(f"{name}'s birthday is {birthday[name]}")
+    else:
+       print(f"Sorry, we don't know {name}")
 
-if name in birthday:
-    print(f"{name}'s birthday is {birthday[name]}")
-else:
-    print(f"Sorry, we don't know {name}")
-    print("Would you like to add it? (yes/no)")
-    answer = input()
-    if answer == "yes":
-        print("What is the birthday?")
-        bday = input()
-        birthday[name] = bday
-        with open("dataFiles/birthdays.json", "w", encoding="utf-8") as file:
-            json.dump(birthday, file)
-        print("Birthday added.")
+def addBirthday():
+    name = input("What is the name? ")
+    bday = input("What is the birthday? ")
+    birthday[name] = bday
+    with open("dataFiles/birthdays.json", "w", encoding="utf-8") as file:
+        json.dump(birthday, file)
+    print("Birthday added.")
+
+while True:
+
+    action = input("What do you want to do? Possible actions are: (A)dd, (F)ind, (L)ist or (Q)uit\n").capitalize()
+
+    if action.startswith("Q"):
+        raise SystemExit(0)
+    elif action.startswith("L"):
+        listNames()
+    elif action.startswith("F"):
+        findAndPrintBirthDay()
+    elif action.startswith("A"):
+        addBirthday()
