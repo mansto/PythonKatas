fdd47a8ed61d5a61263c2b4140eaeb7b36193cd6
Manfred Stocker <manfred.stocker@jetticket.at>
2024-11-04 11:17:46 +0100

    Added part two of birthday katta

--- Birthday.py
+++ Birthday.py
@@ -1,14 +1,10 @@
 # https://www.practicepython.org/exercise/2017/01/24/33-birthday-dictionaries.html
+# https://www.practicepython.org/exercise/2017/02/06/34-birthday-json.html
 
-birthday = {
-    "Hans Maier": "01/01/1990",
-    "Lina Kohlmann": "02/02/1991",
-    "Peter Müller": "03/03/1992",
-    "Anna Schmidt": "04/04/1993",
-    "Hans Schmidt": "05/05/1994",
-    "Lia Schmidt": "06/06/1995",
-    "Peter Hauenmüller": "07/07/1996",
-}
+import json
+
+with open("dataFiles/birthdays.json", "r", encoding="utf-8") as file:
+    birthday = json.load(file)
 
 print("Welcome to the birthday dictionary. We know the birthdays of:")  
 for name in birthday:
@@ -21,3 +17,12 @@ if name in birthday:
     print(f"{name}'s birthday is {birthday[name]}")
 else:
     print(f"Sorry, we don't know {name}")
+    print("Would you like to add it? (yes/no)")
+    answer = input()
+    if answer == "yes":
+        print("What is the birthday?")
+        bday = input()
+        birthday[name] = bday
+        with open("dataFiles/birthdays.json", "w", encoding="utf-8") as file:
+            json.dump(birthday, file)
+        print("Birthday added.")
