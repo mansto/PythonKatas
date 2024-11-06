bc9692f9005c76ce2cde3fea546587018629dbb8
Manfred Stocker <manfred.stocker@jetticket.at>
2024-11-06 09:35:41 +0100

    Refactored birthdays katta

--- Birthday.py
+++ Birthday.py
@@ -1,6 +1,7 @@
 # Part 1: https://www.practicepython.org/exercise/2017/01/24/33-birthday-dictionaries.html
 # Part 2: https://www.practicepython.org/exercise/2017/02/06/34-birthday-json.html
 # Part 3: https://www.practicepython.org/exercise/2017/02/28/35-birthday-months.html
+# Part 4: https://www.practicepython.org/exercise/2017/04/02/36-birthday-plots.html
 
 import json
 import datetime
@@ -10,19 +11,19 @@ from itertools import groupby
 with open("dataFiles/birthdays.json", "r", encoding="utf-8") as file:
     birthday = json.load(file)
 
-def listNames():
+def list():
     print("We know the birthdays of:")
     for name in birthday:
         print(name)
 
-def findAndPrintBirthDay():
+def find():
     name = input("Who's birthday do you want to find: ")
     if name in birthday:
        print(f"{name}'s birthday is {birthday[name]}")
     else:
        print(f"Sorry, we don't know {name}")
 
-def addBirthday():
+def add():
     name = input("What is the name? ")
     bday = input("What is the birthday? (DD.MM.YYYY) ")
     birthday[name] = datetime.datetime.strptime(bday, "%d.%m.%Y").strftime("%d.%m.%Y")
@@ -30,23 +31,41 @@ def addBirthday():
         json.dump(birthday, file)
     print("Birthday added.")
 
-def listBirthdaysPerMonth():
+def groupByMonth():
     months = [datetime.datetime.strptime(birthday[name], "%d.%m.%Y").strftime("%B") for name in birthday]
     months.sort()
     c = Counter(months)
-    print(c)
+    for month, count in c.items():
+        print(f"{month}: {count}")
 
-while True:
+def plot():
+    print("Not implemented yet")
 
-    action = input("What do you want to do? Possible actions are: (A)dd, (F)ind, (L)ist, List (b)irthdays per month, or (Q)uit\n").capitalize()
+def main():
+    while True:
+        print()
+        print("a. add")
+        print("f. find")
+        print("l. list")
+        print("g. group by month")
+        print("p. plot")
+        print("q. Quit")
+        print()
 
-    if action.startswith("Q"):
-        raise SystemExit(0)
-    elif action.startswith("L"):
-        listNames()
-    elif action.startswith("F"):
-        findAndPrintBirthDay()
-    elif action.startswith("A"):
-        addBirthday()
-    elif action.startswith("B"):
-        listBirthdaysPerMonth()
+        action = input("What do you want to do? ").capitalize()
+
+        if action.startswith("Q"):
+            raise SystemExit(0)
+        elif action.startswith("L"):
+            list()
+        elif action.startswith("F"):
+            find()
+        elif action.startswith("A"):
+            add()
+        elif action.startswith("G"):
+            groupByMonth()
+        elif action.startswith("P"):
+            plot()
+
+if __name__ == "__main__":
+    main()
